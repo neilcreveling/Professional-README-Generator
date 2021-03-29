1a0d2e4468cf58d82a30f2e01e4d8953ed932ff5
Neil Creveling <neil.creveling@gmail.com>
2021-03-29 15:16:22 -0400

    added inquirer prompts to index.js

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'Please provide a description.',
+      },
+      {
+        type: 'input',
+        name: 'Installation',
+        message: 'Please provide installation instructions.',
+      },
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'Please provide usage instructions.',
+      },
+      {
+        type: 'input',
+        name: 'Contributing',
+        message: 'Please provide contribution guidelines.',
+      },
+      {
+        type: 'input',
+        name: 'Tests',
+        message: 'Please provide test instructions.',
+      },
+      {
+        type: 'list',
+        name: 'License',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD'],
+        message: 'Enter your LinkedIn URL.',
+      },
+    ]);
+  };
