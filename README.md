745342119ee8031f48e709bfb637ccbac7d3fe28
Neil Creveling <neil.creveling@gmail.com>
2021-04-01 16:18:13 -0400

    deleted internal modules, created logic for generating license badges

--- README.md
+++
@@ -1,2 +0,0 @@
-# Professional-README-Generator
-You can quickly and easily create a README file by using a command-line application to generate one. This allows the project creator to devote more time to working on the project.
--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const fs = require('fs');
 const util = require('util');
 const inquirer = require('inquirer');
 
-// internal packages
-const usernameAPI = require('./utils/username-api.js');
-
+// create writeFile function using promises instead of callbacks
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // user prompts
 const promptUser = () => {
@@ -59,12 +58,74 @@ const promptUser = () => {
     ]);
   };
 
-  // function to write README file
-  function writeToFile(fileName, data) {
-      fs.writeFile(fileName, data, err => {
-          if (err) {
-              return console.log(err);
-          }
-          console.log('Success!')
-      });
+  if (userResponse.license === 'MIT') {
+    licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
   }
+  
+  if (userResponse.license === 'Apache') {
+    licenseBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }
+
+  if (userResponse.license === 'GPL') {
+    licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)'
+  }
+
+  if (userResponse.license === 'BSD') {
+    licenseBadge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+  }
+
+ const generateREADME = (userResponse) =>
+    `## ${userResponse.title}
+    \n
+    ${userResponse.description}
+    \n
+    ${licenseBadge}
+    \n
+    ## Table of Contents
+    \n
+    * Installation
+    \n
+    * Instructions
+    \n
+    * Contributors
+    \n
+    * Tests
+    \n
+    * Contact
+    \n
+    # Installation
+    \n
+    ${userResponse.installation}
+    \n
+    # Instructions
+    \n
+    ${userResponse.usage}
+    \n
+    # Contributing
+    \n
+    ${userResponse.contributing}
+    \n
+    # Tests
+    \n
+    ${userResponse.tests}
+    \n
+    # License
+    \n
+    This project is licensed under the ${userResponse.license} license.
+    \n
+    # Contact
+    \n
+    [${userResponse.github}](github.com/${userResponse.github})
+    \n
+    ${userResponse.email}`
+
+
+  const init = () => {
+      promptUser()
+        .then((userResponse) => writeFileAsync('README.md', generateREADME(userResponse)))
+        .then(() => console.log("Success!"))
+        .catch((err) => console.error(err))
+  }
+
+    init();
+  
