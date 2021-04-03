0b977318ed743ed3d50dd354d7d35c2dfcb41a6c
Neil Creveling <neil.creveling@gmail.com>
2021-04-03 09:00:07 -0400

    created badges.js file

---
+++ badges.js
@@ -0,0 +1,12 @@
+const mit = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+
+const apache = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+
+const gpl = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)'
+
+const bsd = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+
+module.exports.mit = mit;
+module.exports.apache = apache;
+module.exports.gpl = gpl;
+module.exports.bsd = bsd
